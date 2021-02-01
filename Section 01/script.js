2543fd17d71e7f9e97bf966c05ce4e834909cc66
HoussemBoubakour <houssemboubakour@gmail.com>
2021-02-01 18:05:26 -0500

    Edit 02/01

---
+++ Section 01/script.js
@@ -0,0 +1,130 @@
+/*
+const Mheight = 1.69;
+const JHeight = 1.94;
+const MWeight = 78;
+const JWeight = 92;
+
+function calculateBMI(height, weight){
+    let BMI;
+    BMI = weight / (height*height);
+    return BMI;
+};
+
+const markBMI = calculateBMI(Mheight,MWeight);
+const johnBMI = calculateBMI(JHeight,JWeight);
+
+    let markHeigherthanJohn;
+
+if(johnBMI < markBMI){
+    markHeigherthanJohn = true;
+}else{
+    markHeigherthanJohn = false;
+};
+
+if (markHeigherthanJohn===true){
+console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
+}else{
+    console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
+}
+
+
+
+
+// CODING CHALLENGE 03
+
+const AVG_Dolphines = (96+108+89)/3;
+const AVG_Koalas = (88+91+110)/3;
+
+console.log (`Dolphines = ${AVG_Dolphines} Koalas = ${AVG_Koalas}`);
+
+if (AVG_Dolphines>AVG_Koalas){
+    console.log(`Dolphines win the trophy`);
+}else if(AVG_Koalas>AVG_Dolphines){
+    console.log(`Koalas win the trophy`);
+}else{
+    console.log(`it's a DRAW!!`);
+}
+
+// BONUS 01
+
+const AVG_Dolphines1 = (97+112+101)/3;
+const AVG_Koalas1 = (109+95+123)/3;
+
+
+console.log (`Dolphines = ${AVG_Dolphines1} Koalas = ${AVG_Koalas1}`);
+
+if ((AVG_Dolphines1>AVG_Koalas1) && (AVG_Dolphines1>=100)){
+    
+        console.log(`Dolphines win the trophy`);
+    
+}else if(AVG_Koalas1>AVG_Dolphines1 && AVG_Koalas1>=100){
+    
+        console.log(`Koalas win the trophy`);
+
+}else{
+
+    console.log(`it's a DRAW!!`);
+
+}
+
+// BONUS 02
+
+const AVG_Dolphines2 = (97+112+101)/3;
+const AVG_Koalas2 = (109+95+106)/3;
+
+
+console.log (`Dolphines = ${AVG_Dolphines2} Koalas = ${AVG_Koalas2}`);
+
+if ((AVG_Dolphines2>AVG_Koalas2) && (AVG_Dolphines2>=100)){
+    
+        console.log(`Dolphines win the trophy`);
+    
+}else if((AVG_Koalas2>AVG_Dolphines2) && (AVG_Koalas2>=100)){
+    
+        console.log(`Koalas win the trophy`);
+
+}else if((AVG_Koalas2===AVG_Dolphines2) && (AVG_Koalas2>=100) && (AVG_Dolphines2>=100)) {
+
+    console.log(`it's a DRAW!!`);
+
+}else{
+    console.log('No team wins the trophy.');
+}
+
+
+
+
+
+// Coding Challenge 04
+
+let bill, tip;
+
+if (bill >= 50 && bill <= 300){
+    tip = bill*15/100;
+} else{
+    tip = bill*20/100;
+}
+bill = 275;
+
+(bill >= 50 && bill <= 300)? tip = (bill*15)/100 : tip = (bill*20)/100 ;
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${bill+tip}`);
+
+//
+
+bill = 40;
+
+(bill >= 50 && bill <= 300)? tip = (bill*15)/100 : tip = (bill*20)/100 ;
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${bill+tip}`);
+
+//
+
+bill = 430;
+
+(bill >= 50 && bill <= 300)? tip = (bill*15)/100 : tip = (bill*20)/100 ;
+
+console.log(`The bill was ${bill}, the tip was ${tip} and the total value is ${bill+tip}`);
+
+*/
+
--- script.js
+++ script.js
@@ -1,97 +1,13 @@
-/*
-const Mheight = 1.69;
-const JHeight = 1.94;
-const MWeight = 78;
-const JWeight = 92;
+'use strict';
 
-function calculateBMI(height, weight){
-    let BMI;
-    BMI = weight / (height*height);
-    return BMI;
-};
 
-const markBMI = calculateBMI(Mheight,MWeight);
-const johnBMI = calculateBMI(JHeight,JWeight);
-
-    let markHeigherthanJohn;
-
-if(johnBMI < markBMI){
-    markHeigherthanJohn = true;
-}else{
-    markHeigherthanJohn = false;
-};
-
-if (markHeigherthanJohn===true){
-console.log(`Mark's BMI (${markBMI}) is higher than John's BMI (${johnBMI})`);
-}else{
-    console.log(`John's BMI (${johnBMI}) is higher than Mark's BMI (${markBMI})`);
+function fruitProcessor (apples, oranges){
+    const juice = `Juice with ${apples} apples and ${oranges} oranges`;
+    return juice;
 }
 
+const appleJuice = fruitProcessor(5,0);
+console.log(appleJuice);
 
-
-
-// CODING CHALLENGE 03
-
-const AVG_Dolphines = (96+108+89)/3;
-const AVG_Koalas = (88+91+110)/3;
-
-console.log (`Dolphines = ${AVG_Dolphines} Koalas = ${AVG_Koalas}`);
-
-if (AVG_Dolphines>AVG_Koalas){
-    console.log(`Dolphines win the trophy`);
-}else if(AVG_Koalas>AVG_Dolphines){
-    console.log(`Koalas win the trophy`);
-}else{
-    console.log(`it's a DRAW!!`);
-}
-
-// BONUS 01
-
-const AVG_Dolphines1 = (97+112+101)/3;
-const AVG_Koalas1 = (109+95+123)/3;
-
-
-console.log (`Dolphines = ${AVG_Dolphines1} Koalas = ${AVG_Koalas1}`);
-
-if ((AVG_Dolphines1>AVG_Koalas1) && (AVG_Dolphines1>=100)){
-    
-        console.log(`Dolphines win the trophy`);
-    
-}else if(AVG_Koalas1>AVG_Dolphines1 && AVG_Koalas1>=100){
-    
-        console.log(`Koalas win the trophy`);
-
-}else{
-
-    console.log(`it's a DRAW!!`);
-
-}
-
-// BONUS 02
-
-const AVG_Dolphines2 = (97+112+101)/3;
-const AVG_Koalas2 = (109+95+106)/3;
-
-
-console.log (`Dolphines = ${AVG_Dolphines2} Koalas = ${AVG_Koalas2}`);
-
-if ((AVG_Dolphines2>AVG_Koalas2) && (AVG_Dolphines2>=100)){
-    
-        console.log(`Dolphines win the trophy`);
-    
-}else if((AVG_Koalas2>AVG_Dolphines2) && (AVG_Koalas2>=100)){
-    
-        console.log(`Koalas win the trophy`);
-
-}else if((AVG_Koalas2===AVG_Dolphines2) && (AVG_Koalas2>=100) && (AVG_Dolphines2>=100)) {
-
-    console.log(`it's a DRAW!!`);
-
-}else{
-    console.log('No team wins the trophy.');
-}
-
-
-*/
-
-
+const appleAndOrangeJuice = fruitProcessor(2,4);
+console.log(appleAndOrangeJuice);
