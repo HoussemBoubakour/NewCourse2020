a74c0406cea152d0909cf02c9089bca7cca35518
HoussemBoubakour <houssemboubakour@gmail.com>
2021-03-02 18:44:15 -0500

    Edit 03/02/21

--- Section 06/Project 03/script.js
+++ Section 06/Project 03/script.js
@@ -1,1 +1,35 @@
 "use strict";
+
+const score0El = document.querySelector("#score--0");
+const score1El = document.querySelector("#score--1");
+const diceEl = document.querySelector(".dice");
+const btnNew = document.querySelector(".btn--new");
+const btnHold = document.querySelector(".btn--hold");
+const btnRoll = document.querySelector(".btn--roll");
+
+score0El.textContent = "0";
+score1El.textContent = "0";
+diceEl.classList.add("hidden");
+
+let currentScore = 0;
+
+btnRoll.addEventListener("click", function () {
+  // Generate a random number
+
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // display dice
+
+  diceEl.classList.remove("hidden");
+  diceEl.src = `dice-${dice}.png`;
+  console.log(dice);
+
+  // check if it's 1.
+  if (dice !== 1) {
+    // add to the current score
+    currentScore += dice;
+  } else {
+    //switch to the other player
+    currentScore = 0;
+  }
+});
