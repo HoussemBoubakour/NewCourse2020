2463004b4c48f88b6752c109899703d939fa01e1
HoussemBoubakour <houssemboubakour@gmail.com>
2021-03-05 22:29:01 -0500

    Edit 03/05/21

--- Section 06/Project 03/script.js
+++ Section 06/Project 03/script.js
@@ -1,5 +1,7 @@
 "use strict";
 
+const player0El = document.querySelector(".player--0");
+const player1El = document.querySelector(".player--1");
 const score0El = document.querySelector("#score--0");
 const score1El = document.querySelector("#score--1");
 const current0El = document.getElementById("current--0");
@@ -22,7 +24,10 @@ score1El.textContent = "0";
 const switchPlayer = function () {
   currentScore = 0;
   document.getElementById(`current--${activePlayer}`).textContent = 0;
-  activePlayer === 0 ? (activePlayer = 1) : (activePlayer = 0);
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+  player0El.classList.toggle("player--active");
+  player1El.classList.toggle("player--active");
 };
 
 btnRoll.addEventListener("click", function () {
